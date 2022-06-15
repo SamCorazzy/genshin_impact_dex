162e4ba8c92cf33b3aa4b53ee6d2cae7d29c3b43
Samuel Perez Reyes <corazzy@hotmail.com>
2022-06-15 10:02:46 -0500

    Select se intercambia segun las opciones del primer select

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const tipos ={
+const tipos = {
   artifacts: "Artefactos",
   boss: "Jefes",
   characters: "Personajes",
@@ -11,71 +11,86 @@ const tipos ={
   materials: "Materiales",
   nations: "Naciones",
   weapons: "Armas",
-}
+};
 
 function App() {
 
   const [genshinState, setGenshinState] = useState({
     types: [],
-    }
+  }
   );
 
-
-  const fetchGenshinApi = async (item, url="https://api.genshin.dev/") => {
+  const fetchGenshinApi = async (item, url = "https://api.genshin.dev/") => {
     const respuesta = await fetch(url);
     const respJson = await respuesta.json();
 
-    if(item === "types"){
+  
+
+    if (item === "types") {
       setGenshinState({
         ...genshinState,
         types: respJson.types,
       });
     }
-    else{
+    else {
       setGenshinState({
-        ...genshinState,
-        [item]:respJson,
-      });
+        types:[...genshinState.types],
+         [item]:respJson,
+        });
     }
-    console.log(respJson)
+    console.log(respJson);
   };
-  
+
   useEffect(() => {
     fetchGenshinApi("types");
   }, []);
-    
 
-  const handleChangeType = ({target}) => {
-    const url= `https://api.genshin.dev/${target.value}`;
+
+  const handleChangeType = ({ target }) => {
+    const url = `https://api.genshin.dev/${target.value}`;
     fetchGenshinApi(target.value, url);
-  }
+  };
+
 
 
   return (
-    <div className="App card container p-3 mt-4 div" style={{border:"none"}}>
-      <h1 className="text-center">Genshin Impact</h1> 
+    <div className="App card container p-3 mt-4 div" style={{ border: "none" }}>
+      <h1 className="text-center">Genshin Impact</h1>
       <select className="div" name="types" onChange={handleChangeType}>
         <option value="">Seleccione una opción</option>
-        {genshinState.types.map((type)=>(
-        <option key={type} value={type} >
-          {tipos[type]}
-        </option>
+        {genshinState.types.map((type) => (
+          <option key={type} value={type} >
+            {tipos[type]}
+          </option>
         ))}
       </select>
-          {
-            genshinState.artifacts && (
-            <select className="div mt-2" name="artifacts" id="">
-              <option value="">Seleccione un set de artefacto</option>
-              {genshinState.artifacts.map( (artifact) => (
-                <option key={artifact} value={artifact}>
-                  {artifact}
-                </option>
-              ))}
-            </select>
-            )
-          }
+      {
+        genshinState.artifacts && (
+          <select className="div mt-2" name="artifacts" id="">
+            <option value="">Seleccione un set de artefacto</option>
+            {genshinState.artifacts.map((artifact) => (
+              <option key={artifact} value={artifact}>
+                {artifact}
+              </option>
+            ))}
+          </select>
+        )
+      }
+      {
+        genshinState.boss && (
+          <select className="div mt-2" name="boss" id="">
+            <option value="">Seleccione un set de jefes</option>
+            {genshinState.boss.map((bos) => (
+              <option key={bos} value={bos}>
+                {bos}
+              </option>
+            ))}
+          </select>
+        )
+      }
+
     </div>
-    
+
   );
 }
 
